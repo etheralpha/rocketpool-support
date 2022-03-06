ac9de49d8c2855bef49471a23b9fbd4cc2226948
hanniabu <hanniabu@gmail.com>
2022-03-06 15:30:14 -0500

    only show discord invite for answers

--- _includes/js/base.js
+++ _includes/js/base.js
@@ -1,9 +1,11 @@
 let promptHistory = [1];
+let answerIds = {{ site.data.troubleshooting-prompts | where: "type", "answer" | map: "id" | jsonify }};
 
 function goToPrompt(currentId, nextId) {
   const currentPrompt = document.getElementById("prompt" + currentId);
   const nextPrompt = document.getElementById("prompt" + nextId);
   const backButton = document.getElementById("promptBack");
+  const discordInvite = document.getElementById("promptDiscordInvite");
   // show the back button unless it's the starting prompt
   if (nextId == 1) {
     backButton.classList.add("d-none");
@@ -13,6 +15,12 @@ function goToPrompt(currentId, nextId) {
   // hide the current prompt and show the next one
   currentPrompt.classList.add("d-none");
   nextPrompt.classList.remove("d-none");
+  // only show the discord invite on answer prompts
+  if (answerIds.includes(nextId)) {
+    discordInvite.classList.remove("d-none");
+  } else {
+    discordInvite.classList.add("d-none");
+  }
   // update promptHistory unless going back
   if (promptHistory[promptHistory.length - 1] !== nextId) {
     promptHistory.push(nextId);
